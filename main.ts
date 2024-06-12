343b3e74c4a02c42d9f9b0147150595b173b2d3e
grperry <92124381+grperry@users.noreply.github.com>
2024-06-12 17:10:38 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+IotLoRaNode.SetRegion(region.EU868)
+IotLoRaNode.InitialiseRadio(
+"260B021F",
+"1A15A157343C874D069A0B861D7EC798",
+"AB0F43611CF64269FAFC263C2A521CC0",
+SpreadingFactors.Seven
+)
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.clearScreen()
+    IotLoRaNode.DigitalValue(true, Channels.One)
+    IotLoRaNode.AnalogueValue(26, Channels.Two)
+    IotLoRaNode.TemperatureValue(input.temperature(), Channels.Three)
+    IotLoRaNode.AccelorometerValue(
+    input.acceleration(Dimension.X),
+    input.acceleration(Dimension.Y),
+    input.acceleration(Dimension.Z),
+    Channels.Four
+    )
+    IotLoRaNode.LightValue(input.lightLevel(), Channels.Five)
+    IotLoRaNode.loraTransmitPayload()
+    for (let index = 0; index < 36; index++) {
+        basic.pause(5000)
+    }
+    basic.showIcon(IconNames.Heart)
+})
